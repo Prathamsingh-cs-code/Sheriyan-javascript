83a3815a0e94c7ab010240c51f09bc9bc856b1ad
alok84087@gmail.com <alok84087@gmail.com>
2025-12-07 20:31:42 +0530

    AdvanceJS

--- Day-45-MOREinDOM/script.js
+++ Day-45-MOREinDOM/script.js
@@ -1,20 +1,4 @@
-// Progress bar demo: pick a random percent when the button is clicked
 
-// const btn = document.getElementById('randomize');
-// const inner = document.querySelector('.inner');
-
-// if (btn && inner) {
-// 	btn.addEventListener('click', () => {
-// 		// pick a random value 0..100
-// 		const percent = Math.floor(Math.random() * 101);
-// 		inner.style.width = percent + '%';
-// 		// show the percentage inside the progress bar
-// 		inner.textContent = percent + '%';
-// 	});
-// } else {
-// 	// helpful message for debugging if elements are missing
-// 	console.warn('randomize button or .inner element not found in DOM');
-// }
 var grow = 0
 var btn = document.querySelector("button");
 var h2 = document.querySelector("h2");
@@ -26,7 +10,7 @@ console.log(num);
 
 var int = setInterval(function(){
     grow++
-    inner.style.width = grow + '%';
+     inner.style.width = grow + '%';
     h2.innerHTML = grow + '%';
   }, num);
 
---
+++ Day-57-AdvanceJs/script.js
@@ -0,0 +1,56 @@
+// let obj = {
+//     name: "Gaurav",
+//     age: 21,
+//     greet: function(){
+//         console.log(`Hello, My name is ${this.name} and I am ${this.age} years old.`)
+//     }
+// };
+// console.log(obj)
+
+// class sketch{
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
+// }
+// let alok = new sketch("Gaurav", 21)
+// console.log(alok)
+
+// class alok{
+//     constructor(){
+//         this.name = "Gaurav";
+//         this.age = 21;
+//     }
+// }
+// new alok()
+
+
+// Most Important!
+
+// global -> window -> this
+// function -> window -> this
+// es5 -> function -> this = object
+// es6 -> function -> this = window
+// es5 funtion inside function -> this = object
+// let obj = {
+//     name: "Gaurav",
+//     age: 21,
+//     fun: function(){
+//         function innerFun(){
+//             console.log(this)
+//         }
+//         innerFun()
+//     }
+// };
+// obj.fun()
+// es6 function inside object -> this = window
+// es6 function inside function -> this = window
+
+let obj = {
+    name: "Gaurav",
+    age: 21,
+};
+function fun(){
+    console.log(this)
+}
+fun.call(obj) // this = obj
